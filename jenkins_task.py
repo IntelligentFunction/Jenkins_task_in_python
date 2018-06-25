baade9992a7f7f4cba890735b8f5607d409ccee7
Robert Kinsey <intelligent.function@gmail.com>
2018-06-25 23:11:04 +0100

    create list of parameters and choose random element for function input

--- jenkins_task.py
+++ jenkins_task.py
@@ -11,13 +11,13 @@ channel_dict = {}
 
 def create_channel(channel_type,channel_number):
     print("Creating Channel...")
-    sleep(3)
+    sleep(10)
     print("status: 10% complete")
-    sleep(3)
+    sleep(10)
     print("status...")
-    sleep(3)
-    channel_dict['type'] = "PPV"
-    channel_dict['number'] = 4
+    sleep(10)
+    channel_dict['type'] = channel_type
+    channel_dict['number'] = channel_number
     channel_dict['timestamp'] = datetime.now()
     return channel_dict
 
@@ -26,13 +26,15 @@ def create_channel(channel_type,channel_number):
 if __name__ == "__main__":
 
     # mocked input
+    from random import choice
+    channel_type = ['EFL','PPV','F1']
+    channel_number = [i for i in range(1,12)]
 
-    channel_type = "F1"
-    channel_number = "4"
+    type_choice = choice(channel_type)
+    number_choice = choice(channel_number)
 
-    new_channel = create_channel("PPV",4)
-
-    print(f"channel {new_channel['type']} {new_channel['number']} has completed at {new_channel['timestamp']} ")
+    new_channel = create_channel(type_choice,number_choice)
+    print(f"\n\t Channel {new_channel['type']}, {new_channel['number']} has completed \n\t at {new_channel['timestamp']} ")
 
     
     
